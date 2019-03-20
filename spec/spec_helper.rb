bb97bfcdadb94551569c8970ca394d75e0a85359
BartJudge <BartJudge@gmail.com>
2019-03-20 13:02:35 +0000

    listing all bookmarks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
-
 ENV['RACK_ENV'] = 'test'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+
 Capybara.app = BookmarkManager
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
