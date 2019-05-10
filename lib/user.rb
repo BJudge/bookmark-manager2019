616ce2f61d6e6baa5c2a0792c6348edfff5975e6
BartJudge <BartJudge@gmail.com>
2019-05-11 00:46:27 +0100

    bookmark manager complete

--- lib/user.rb
+++ lib/user.rb
@@ -17,6 +17,13 @@ class User
     @email = email
   end
 
+  def self.authenticate(email:, password:)
+    result = DatabaseConnection.query("SELECT * FROM users WHERE email = '#{email}'")
+    return unless result.any?
+    return unless BCrypt::Password.new(result[0]['password']) == password
+    User.new(id: result[0]['id'], email: result[0]['email'])
+  end
+
   def self.find(id)
     return nil unless id
     result = DatabaseConnection.query("SELECT * FROM users WHERE id = #{id}")
